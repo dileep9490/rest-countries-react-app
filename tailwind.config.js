ac82993dbccf3446d6569832598ff533077a37f8
dileep <dileepgudimetla9490@gmail.com>
2022-10-13 16:06:52 +0530

    configured styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"dark-blue-d": "#2b3945",
+				"very-dark-blue-d": "#202c37",
+				"very-dark-blue-l": "#111517",
+				"dark-grey-l": "#858585",
+				"very-light-grey-l": "#fafafa",
+			},
+			fontFamily: {
+				nunito: ["Nunito Sans", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
