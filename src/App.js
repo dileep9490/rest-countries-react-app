b04e805ad6edaa5621abfbe44af9029707af4c34
dileep <dileepgudimetla9490@gmail.com>
2022-10-15 12:24:11 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Navbar from "./components/Navbar";
+import CountryPage from "./pages/CountryPage";
+import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
-	return <h1>Hello world</h1>;
+	return (
+		<>
+			<Navbar />
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/country" element={<CountryPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/CountryPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CountryPage = () => {
+  return (
+    <div>CountryPage</div>
+  )
+}
+
+export default CountryPage
